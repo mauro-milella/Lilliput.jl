78beb813c540362e00d600f2914576c05004194f
mauro-milella <milella_mauro@live.it>
2026-07-08 23:58:55 +0200

    [ADD] _utf8_or_replacementchar! as an alternative to Base.isvalid

--- src/tokenizers/utilities.jl
+++ src/tokenizers/utilities.jl
@@ -81,6 +81,7 @@ function merge(
     return new_indexes[1:(j - 1)]
 end
 
+# this could leverage Base.Unicode.iscntrl internally
 function _replace_control_characters(s::AbstractString)
     # https://github.com/JuliaLang/julia/blob/master/base/strings/unicode.jl
     # http://www.unicode.org/reports/tr44/#GC_Values_Table
@@ -102,6 +103,104 @@ function _replace_control_characters(s::AbstractString)
     return join(chars)
 end
 
+
+"""
+Check whether the given bytes are a valid UTF-8 string and return them.
+
+If it is not the case, return the replacement Unicode character `\uFFFD` (�).
+
+See [`Base.isvalid`](@ref),
+[replacement character](https://en.wikipedia.org/wiki/Specials_(Unicode_block)).
+"""
+function _utf8_or_replacementchar!(bytes::AbstractVector{UInt8})
+    io = IOBuffer()
+    i = firstindex(bytes)
+    n = lastindex(bytes)
+
+    while i <= n
+        b = bytes[i]
+
+        # ASCII check; of course, 0x80 is UInt8(128)
+        if b < 0x80
+            write(io, Char(b))
+            i += 1
+            continue
+        end
+
+        # to read UTF-8, we need to check the sequence length.
+        # 0******* -> 1 byte sequence (ASCII, covered above)
+        # 110***** -> 2 bytes 
+        # 1110**** -> 3 bytes
+        # 11110*** -> 4 bytes
+        # 10****** -> continuation of a 2/3/4 bytes sequence
+        # https://en.wikipedia.org/wiki/UTF-8
+        len =
+            # 0xe0 is exactly 1<<7 + 1<<6 + 1<<5, that is, 11100000
+            # we use this mask over b, and see if the result is 11000000
+            # (or 192, which is in fact 1<<7 + 1<<6 and no 1<<5)
+            b & 0xe0 == 0xc0 ? 2 :
+            # same reasoning as above
+            b & 0xf0 == 0xe0 ? 3 :
+            b & 0xf8 == 0xf0 ? 4 : 0
+
+        # the default "error" scenario above is 0, so this is invalid
+        if len == 0
+            write(io, '\uFFFD')
+            i += 1
+            continue
+        end
+
+        # the sequence is tuncated, because `len` tells that we have to 
+        # overpass the boundary n 
+        if i + len - 1 > n
+            write(io, '\uFFFD')
+            break
+        end
+
+        # these are the useful bits from the first byte;
+        # we consider UInt32 because we can potentially encode 4 bytes
+        code = UInt32(b & (0x7f >> len))
+        valid = true
+
+        for j in 2:len
+            c = bytes[i + j - 1]
+            # 10****** is the continuation character
+            if c & 0xc0 != 0x80
+                valid = false
+                break
+            end
+            # keep the low 6 bits with 0x3f and shift them to make room for more
+            code = (code << 6) | UInt32(c & 0x3f)
+        end
+
+        valid &= (
+            # if the length is 2 and you are using more less than 2 bytes, bad
+            (len == 2 && code >= 0x80) ||
+            (len == 3 && code >= 0x800) ||
+            (len == 4 && code >= 0x10000)
+        )
+        # anything bigger than this is not a valid Unicode
+        # see the Error handling section here
+        # https://en.wikipedia.org/wiki/UTF-8
+        valid &= code <= 0x10ffff
+
+        # see the Surrogates section below; these are not legal Unicode values
+        # https://en.wikipedia.org/wiki/UTF-8
+        valid &= !(0xd800 <= code <= 0xdfff)
+
+        if valid
+            write(io, Char(code))
+        else
+            write(io, '\uFFFD')
+        end
+
+        # Consume the whole sequence, valid or not
+        i += len
+    end
+
+    return String(take!(io))
+end
+
 """
     function render(_token::Vector{UInt8})
 
@@ -110,20 +209,5 @@ Bytes are converted in na string, where Unicode sequences that are dangerous
 to print (e.g., '\n') are escaped.
 """
 function render(_tokens::AbstractVector{UInt8})
-    # see https://github.com/JuliaLang/julia/blob/master/base/strings/unicode.jl
-    s = nothing
-
-    try
-        # not everything can be converted to UTF-8; this is a lossy encoding 
-        # to a human-readable form of raw bytes
-        s = Base.Unicode.normalize(String(copy(_tokens)), :NFKD)
-    catch
-        # we fallback to the replacement character
-        # https://en.wikipedia.org/wiki/Specials_(Unicode_block)
-        s = "�"
-    end
-
-    c = _replace_control_characters(s)
-
-    return c
+    return _replace_control_characters(_utf8_or_replacementchar!(_tokens))
 end
